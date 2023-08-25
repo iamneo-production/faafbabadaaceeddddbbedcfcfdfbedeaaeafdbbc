52f3de07ef67b76f9a24b79589a3ae84092aab09
Iamneo <techadmin@iamneo.ai>
2023-08-25 13:09:03 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -39,13 +39,12 @@ return (
 <h1 className="text-3xl">Quizz App</h1>
 <h1 style={{textAlign:'center'}}>learn react</h1>
 </div>
-{/* {start && <Button onClick={ handleStart} style={"bg-orange-
-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button> } */}
 {banner && <Banner score={attempt} />}
 {card && <div> {questions.map((q, index) => (
-<Card key={index} question={q.question} correctAnswerMarkUpdate={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer}setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsnAttempt} />))}
+  <Card key={index} question={q.question} correctAnswerMarkUpdate={correctAnswerMarkUpdate} attempt={attempt} options={opt} answer={q.answer}setQsns={setqsnAttempt} qsn={ qsnAttempt} setCard={setResult} qsnAttempt={qsnAttempt} />))}
 </div>}
 {result && <Button onClick={handleResult} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400 mt-3"} >Show Results</Button>}
+{/* {start && <Button onClick={ handleStart} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button> } */}
 {start && <Button onClick={ handleStart} style={"bg-orange-300 px-10 py-2 rounded hover:bg-orange-400"}>Start Quiz</Button> }
 </div>
 </div>
